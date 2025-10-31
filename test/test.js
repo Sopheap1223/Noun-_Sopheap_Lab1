73e0747bfec37102f4f40032daf584419c223ee3
Sopheap Noun <snoun1223@gmail.com>
2025-10-31 11:28:02 +0700

    test PR

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../src/app');
 const { expect } = require('chai');
 describe('GET /', () => {
-it('should return Hello, GitHub Actions!', async () => {
+it('should return Hello, Pull request', async () => {
 const res = await request(app).get('/');
 expect(res.status).to.equal(200);
-expect(res.text).to.equal('Hello, CI/CD!');
+expect(res.text).to.equal('Hello, Pull request');
 });
 });
